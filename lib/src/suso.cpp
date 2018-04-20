be90189adb6ad95900d3d730ab7dadf69202dea7
Felix Maurer <felix@felix-maurer.de>
2018-04-20 11:54:33 +0200

    Start Sudoku class
    To make implementation of solving algorithms possible we do now have a
    Sudoku class with the definitions of the functions.

--- lib/src/suso.cpp
+++ lib/src/suso.cpp
@@ -2,6 +2,24 @@
 
 #include "suso.h"
 
-void foo() {
-    std::cout << "Hello from library!" << std::endl;
+Sudoku::Sudoku() {
+    field = {{{5, 3, 0, 0, 7, 0, 0, 0, 0},
+                     {6, 0, 0, 1, 9, 5, 0, 0, 0},
+                     {0, 9, 8, 0, 0, 0, 0, 6, 0},
+                     {8, 0, 0, 0, 6, 0, 0, 0, 3},
+                     {4, 0, 0, 8, 0, 3, 0, 0, 1},
+                     {7, 0, 0, 0, 2, 0, 0, 0, 6},
+                     {0, 6, 0, 0, 0, 0, 2, 8, 0},
+                     {0, 0, 0, 4, 1, 9, 0, 0, 5},
+                     {0, 0, 0, 0, 8, 0, 0, 7, 9}}};
+}
+
+std::ostream &operator<<(std::ostream &stream, const Sudoku &sudoku) {
+    for (const auto& line : sudoku.field) {
+        for (const auto& cell : line) {
+            stream << cell << "  ";
+        }
+        stream << std::endl;
+    }
+    return stream;
 }
