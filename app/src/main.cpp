74b73cfec7ba283cfb07877f00fe8b81277cf363
markuskr97 <mkrahl@hm.edu>
2018-05-02 15:02:47 +0200

    ValidNumbers funktioniert implementiert, Test folgen noch, weil ich das Problem mit dem Compiler für die Test-Bibliothek noch nicht fixen konnte #13

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -5,5 +5,11 @@
 int main () {
     Sudoku sudoku;
     std::cout << sudoku << std::endl;
+
+    struct position pos;
+    pos.x = 2;
+    pos.y = 3;
+
+    sudoku.validNumbers(pos);
     return 0;
 }
--- lib/src/suso.cpp
+++ lib/src/suso.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <set>
 
 #include "suso.h"
 
@@ -22,4 +23,39 @@ std::ostream &operator<<(std::ostream &stream, const Sudoku &sudoku) {
         stream << std::endl;
     }
     return stream;
+}
+
+std::vector<int> Sudoku::validNumbers(position pos) {
+    std::set<int> posNumbers;
+    std::vector<int> res;
+
+    for(int i = 0; i<9; i++){
+        posNumbers.insert(i+1);
+    }
+
+    for(int j = 0; j< 9; j++){
+        posNumbers.erase(field[j][pos.y]);
+    }
+
+    for(int k = 0; k< 9; k++){
+        posNumbers.erase(field[pos.x][k]);
+    }
+
+    int xBlock = (pos.x / 3) * 3;
+    int yBlock = (pos.y / 3) *3;
+
+    for (int l = 0; l < 3; l++){
+        for(int m = 0; m < 3; m++){
+            posNumbers.erase(field[xBlock + l][yBlock + m]);
+        }
+    }
+
+    for (std::set<int>::iterator it=posNumbers.begin(); it!=posNumbers.end(); ++it){
+        res.push_back(*it);
+    }
+
+    for (std::vector<int>::iterator iter = res.begin(); iter != res.end(); ++iter)
+        std::cout << *iter << ' ';
+
+    return res;
 }
