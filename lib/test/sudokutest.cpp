b5b4f8baf28913276db62cf9666fcbe9cf2be779
markuskr97 <mkrahl@hm.edu>
2018-05-07 15:58:14 +0200

    Test für solveNakedSingles

--- lib/test/sudokutest.cpp
+++ lib/test/sudokutest.cpp
@@ -1,10 +1,37 @@
 //
 // Created by Markus on 24.04.2018.
 #include "gtest/gtest.h"
+#include "suso.h"
 
-TEST(testMath, myCubeTest) {
-    EXPECT_EQ(1000, 1000);
+TEST(susoTest, validNumbersTest) {
+    Sudoku sudoku;
+    struct position pos;
+    pos.x = 4;
+    pos.y = 4;
+    EXPECT_EQ(sudoku.validNumbers(pos).size(), 1);
+    EXPECT_EQ(sudoku.validNumbers(pos)[0], 5);
+    pos.x = 2;
+    pos.y = 0;
+    EXPECT_EQ(sudoku.validNumbers(pos).size(), 2);
+    EXPECT_EQ(sudoku.validNumbers(pos)[0], 1);
+    EXPECT_EQ(sudoku.validNumbers(pos)[1], 2);
+    pos.x = 0;
+    pos.y = 8;
+    EXPECT_EQ(sudoku.validNumbers(pos).size(), 3);
+    EXPECT_EQ(sudoku.validNumbers(pos)[0], 2);
+    EXPECT_EQ(sudoku.validNumbers(pos)[1], 4);
+    EXPECT_EQ(sudoku.validNumbers(pos)[2], 8);
 }
 
-//
+TEST(susoTest, solveNakedSingleTest) {
+    Sudoku sudoku;
+    bool changed;
+    changed = sudoku.solveNakedSingles();
+    EXPECT_EQ(changed, true);
+    changed = sudoku.solveNakedSingles();
+    EXPECT_EQ(changed, true);
+    changed = sudoku.solveNakedSingles();
+    EXPECT_EQ(changed, true);
+}
+
 
