35b9a06f3ba0ee0231a53d1f9d5752edca8463f9
markuskr97 <mkrahl@hm.edu>
2018-05-07 12:09:10 +0200

    Added tests #13

--- lib/test/sudokutest.cpp
+++ lib/test/sudokutest.cpp
@@ -1,10 +1,27 @@
 //
 // Created by Markus on 24.04.2018.
 #include "gtest/gtest.h"
+#include "suso.h"
 
-TEST(testMath, myCubeTest) {
-    EXPECT_EQ(1000, 1000);
+TEST(susoTest, validNumbersTest) {
+    Sudoku sudoku;
+    struct position pos;
+    pos.x = 4;
+    pos.y = 4;
+    EXPECT_EQ(sudoku.validNumbers(pos).size(), 1);
+    EXPECT_EQ(sudoku.validNumbers(pos)[0], 5);
+    pos.x = 2;
+    pos.y = 0;
+    EXPECT_EQ(sudoku.validNumbers(pos).size(), 2);
+    EXPECT_EQ(sudoku.validNumbers(pos)[0], 1);
+    EXPECT_EQ(sudoku.validNumbers(pos)[1], 2);
+    pos.x = 0;
+    pos.y = 8;
+    EXPECT_EQ(sudoku.validNumbers(pos).size(), 3);
+    EXPECT_EQ(sudoku.validNumbers(pos)[0], 2);
+    EXPECT_EQ(sudoku.validNumbers(pos)[1], 4);
+    EXPECT_EQ(sudoku.validNumbers(pos)[2], 8);
 }
 
-//
+
 
