6bd2f1d45bf073bd9c275779a3e8a82808ff0b27
Felix Maurer <felix@felix-maurer.de>
2018-05-10 20:01:00 +0200

    Fix wrong order of coordinates
    To access a specific cell, it has to be addressed like field[y][x]. Note
    that the order of the coordinates if different from the usual way.
    
    This was not correctly done in our implementations. To prevent this from
    happening in the future, we should use functions that capsulate this
    behavior in as much places as possible.

--- lib/src/suso.cpp
+++ lib/src/suso.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <set>
 
 #include "suso.h"
@@ -13,50 +14,53 @@ std::ostream &operator<<(std::ostream &stream, const Sudoku &sudoku) {
     return stream;
 }
 
+bool Sudoku::isEmpty(position pos) {
+    return field[pos.y][pos.x] == 0;
+}
+
+void Sudoku::insertNumber(position pos, int num) {
+    field[pos.y][pos.x] = num;
+}
+
 std::vector<int> Sudoku::validNumbers(position pos) {
-    std::set<int> posNumbers;
-    std::vector<int> res;
-
-    for (int i = 0; i < 9; i++) {
-        posNumbers.insert(i + 1);
-    }
-
-    for (int j = 0; j < 9; j++) {
-        posNumbers.erase(field[j][pos.y]);
-    }
-
-    for (int k = 0; k < 9; k++) {
-        posNumbers.erase(field[pos.x][k]);
-    }
-
+    std::set<int> res;
     int xBlock = (pos.x / 3) * 3;
     int yBlock = (pos.y / 3) * 3;
+    int x, y;
 
-    for (int l = 0; l < 3; l++) {
-        for (int m = 0; m < 3; m++) {
-            posNumbers.erase(field[xBlock + l][yBlock + m]);
+    for (int i = 1; i <= 9; i++) {
+        res.insert(i);
+    }
+
+    for (x = 0; x < 9; x++) {
+        res.erase(field[pos.y][x]);
+    }
+
+    for (y = 0; y < 9; y++) {
+        res.erase(field[y][pos.x]);
+    }
+
+    for (x = 0; x < 3; x++) {
+        for (y = 0; y < 3; y++) {
+            res.erase(field[yBlock + y][xBlock + x]);
         }
     }
 
-    for (std::set<int>::iterator it = posNumbers.begin(); it != posNumbers.end(); ++it) {
-        res.push_back(*it);
-    }
-    return res;
+    return std::vector<int>(res.begin(), res.end());
 }
 
 bool Sudoku::solveNakedSingles() {
     bool changed = false;
     struct position pos;
 
-    for (int k = 0; k < 9; k++) {
-        for (int l = 0; l < 9; l++) {
-            pos.x = k;
-            pos.y = l;
-            std::vector<int> res = validNumbers(pos);
-
-            if (res.size() == 1 && field[k][l] == 0) {
-                field[k][l] = res[0];
-                changed = true;
+    for (pos.x = 0; pos.x < 9; pos.x++) {
+        for (pos.y = 0; pos.y < 9; pos.y++) {
+            if (isEmpty(pos)) {
+                std::vector<int> res = validNumbers(pos);
+                if (res.size() == 1) {
+                    insertNumber(pos, res[0]);
+                    changed = true;
+                }
             }
         }
     }
