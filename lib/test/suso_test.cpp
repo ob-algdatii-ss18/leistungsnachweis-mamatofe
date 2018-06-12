c705b3149e4eb6dc04b2c0aeb14dc661f0a8458c
Marita Segl <maritasegl@gmx.de>
2018-06-12 12:40:22 +0200

    Update existing tests and use Sukokus from file

--- lib/test/suso_test.cpp
+++ lib/test/suso_test.cpp
@@ -4,6 +4,7 @@
 
 TEST(susoTest, validNumbersTest) {
     Sudoku sudoku;
+    sudoku.updateSudoku("./../../../testdata/sudoku.csv");
     struct position pos;
     pos.x = 4;
     pos.y = 4;
@@ -34,15 +35,8 @@ TEST(susoTest, solveNakedSingleTest) {
 }
 
 TEST(susoTest, checkSolvability) {
-    Sudoku sudoku = Sudoku({{{5, 3, 3, 0, 7, 0, 0, 0, 0},
-                                    {6, 0, 0, 1, 9, 5, 0, 0, 0},
-                                    {0, 9, 8, 0, 0, 0, 0, 6, 0},
-                                    {8, 0, 0, 0, 6, 0, 0, 0, 3},
-                                    {4, 0, 0, 8, 0, 3, 0, 0, 1},
-                                    {7, 0, 0, 0, 2, 0, 0, 0, 6},
-                                    {0, 6, 0, 0, 0, 0, 2, 8, 0},
-                                    {0, 0, 0, 4, 1, 9, 0, 0, 5},
-                                    {0, 0, 0, 0, 8, 0, 0, 7, 9}}});
+    Sudoku sudoku;
+    sudoku.updateSudoku("./../../../testdata/checkSolvability.csv");
     EXPECT_THROW(sudoku.checkSolvability(), std::string);
 }
 
@@ -75,17 +69,10 @@ TEST(susoTest, updateSudokuTest) {
 
 TEST(susoTest, solveBacktracking) {
     Sudoku sudoku;
+    sudoku.updateSudoku("./../../../testdata/sudoku.csv");
     bool solved = sudoku.solveBacktracking();
     EXPECT_EQ(solved, true);
-    std::array<std::array<int, 9>, 9> solutionField = {{{5, 3, 4, 6, 7, 8, 9, 1, 2},
-                                                               {6, 7, 2, 1, 9, 5, 3, 4, 8},
-                                                               {1, 9, 8, 3, 4, 2, 5, 6, 7},
-                                                               {8, 5, 9, 7, 6, 1, 4, 2, 3},
-                                                               {4, 2, 6, 8, 5, 3, 7, 9, 1},
-                                                               {7, 1, 3, 9, 2, 4, 8, 5, 6},
-                                                               {9, 6, 1, 5, 3, 7, 2, 8, 4},
-                                                               {2, 8, 7, 4, 1, 9, 6, 3, 5},
-                                                               {3, 4, 5, 2, 8, 6, 1, 7, 9}}};
-    Sudoku solvedSudoku = Sudoku(solutionField);
+    Sudoku solvedSudoku;
+    solvedSudoku.updateSudoku("./../../../testdata/solvedSudoku.csv");
     EXPECT_EQ(sudoku == solvedSudoku, true);
 }
